9020abc7c8c44dc08ec9f5aa9870174a1d04d049
xinyu <yu.life@qq.com>
2026-01-15 17:07:55 +0800

    初始化提交

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    Main
+
+    <el-button type="primary">
+      <el-icon>
+        <Edit />
+      </el-icon>
+    </el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
