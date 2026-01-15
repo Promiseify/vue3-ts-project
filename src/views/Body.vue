ecd58e1253315b82439d5d002f84206f77be0e34
xinyu <yu.life@qq.com>
2026-01-15 17:27:32 +0800

    新增body组件

---
+++ src/views/Body.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    Main
+
+    <el-button type="primary">
+      <el-icon>
+        <Edit />
+      </el-icon>
+    </el-button>
+    ceshi 
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
